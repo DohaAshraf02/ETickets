8b18f4a4fb46913ee3801db5dcbbb34a81ae1aeb
Doha Ashraf <150551754+DohaAshraf02@users.noreply.github.com>
2025-06-28 23:19:55 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using ETickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-K1HI8GRN\\SQLEXPRESS;Initial Catalog=ETicketsDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure relationships and other model settings here if needed
+            modelBuilder.Entity<Movie>()
+                 .HasOne(m => m.Cinema)
+                 .WithMany(c => c.Movies)
+                 .HasForeignKey(m => m.CinemaId);
+            modelBuilder.Entity<Movie>()
+                .HasOne(m => m.Category)
+                .WithMany(c => c.Movies)
+                .HasForeignKey(m => m.CategoryId);
+
+            modelBuilder.Entity<ActorMovie>()
+                .HasKey(am => new { am.ActorId, am.MovieId });
+            modelBuilder.Entity<ActorMovie>()
+                .HasOne(am => am.Actor)
+                .WithMany(a => a.ActorMovies)
+                .HasForeignKey(am => am.ActorId);
+            modelBuilder.Entity<ActorMovie>()
+                .HasOne(am => am.Movie)
+                .WithMany(m => m.ActorMovies)
+                .HasForeignKey(am => am.MovieId);
+
+
+
+        }
+    }
+
+}
---
+++ Migrations/20250628201305_RenameImageURLAndAddMovieStatusToMoviesTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameImageURLAndAddMovieStatusToMoviesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Movies",
+                newName: "ImgUrl");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovieStatus",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MovieStatus",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "ImgUrl",
+                table: "Movies",
+                newName: "ImageUrl");
+        }
+    }
+}
